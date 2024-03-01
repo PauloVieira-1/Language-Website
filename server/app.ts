929856cd518be7d7795bd4322acfac13185d142a
Paulo Vieira <paulo1vieira.2004@gmail.com>
2024-03-01 23:43:23 +0100

    fixing mongodb ("langauge")

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,6 @@
 import express, { Response } from "express";
 import createError from "http-errors";
+import cookieParser from "cookie-parser";
 import path from "path";
 
 import Config from "./utils/config";
@@ -11,30 +12,27 @@ const config = new Config();
 
 const app = express();
 
-// Start of my nonsense 
+// Start of my nonsense
 
-const mongoose = require('mongoose'); 
-const bodyParser = require('body-parser');
+import mongoose from "mongoose";
 
-app.use(bodyParser.urlencoded({extended: true}));
-
-// End of my nonsense 
+// End of my nonsense
 
 if (config.isDev) {
-	const livereload = require("livereload");
-	const connectLiveReload = require("connect-livereload");
+  const livereload = require("livereload");
+  const connectLiveReload = require("connect-livereload");
 
-	const liveReloadServer = livereload.createServer({
-		exts: ["html", "css", "js", "ts", "pug"],
-	});
-	for (const dir of ["public", "views", "content", "server"]) {
-		liveReloadServer.watch(path.join(__dirname, "..", dir));
-	}
+  const liveReloadServer = livereload.createServer({
+    exts: ["html", "css", "js", "ts", "pug"],
+  });
+  for (const dir of ["public", "views", "content", "server"]) {
+    liveReloadServer.watch(path.join(__dirname, "..", dir));
+  }
 
-	liveReloadServer.server.once("connection", () =>
-		setTimeout(() => liveReloadServer.refresh("/"), 100)
-	);
-	app.use(connectLiveReload());
+  liveReloadServer.server.once("connection", () =>
+    setTimeout(() => liveReloadServer.refresh("/"), 100)
+  );
+  app.use(connectLiveReload());
 }
 
 app.set("views", path.join(__dirname, "..", "views"));
@@ -42,69 +40,62 @@ app.set("view engine", "pug");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(express.static(path.join(__dirname, "..", "content")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
+// My Nonsense Mongodb Stuff
+
+mongoose
+  .connect(config.mongoDbConnetcionString)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+
+const notesSchema = new mongoose.Schema({
+  word: String,
+  language: String,
+  gender: String,
+});
+
+const Note = mongoose.model("Note", notesSchema);
+
+app.post("/", function (req, res) {
+  let newNote = new Note({
+    word: req.body.word,
+    language: req.body.language,
+    gender: req.body.gender,
+  });
+
+  newNote.save();
+  res.redirect("/");
+});
+
+// End of my nonsense
+
 app.use((req, res, next) => {
-	next(createError(404));
+  next(createError(404));
 });
 
 app.use((err: createError.HttpError, req, res: Response, next) => {
-	res.locals.message = err.message;
-	res.locals.error = config.isDev ? err : {};
+  res.locals.message = err.message;
+  res.locals.error = config.isDev ? err : {};
 
-	res.status(err.status || 500);
-	res.render("error");
+  res.status(err.status || 500);
+  res.render("error");
 });
 
-// My Nonsense Mongodb Stuff 
-
-	mongoose.connect('mongodb://localhost:27017', {
-	  useNewUrlParser: true,
-	  useUnifiedTopology: true
-	})
-	.then(() => {
-	  console.log('Connected to MongoDB');
-	})
-	.catch((err) => {
-	  console.error('Error connecting to MongoDB:', err);
-	});
-
-	const notesSchema = new mongoose.Schema({
-	    word: String,
-	    Langauge: String,
-	    Gender: String
-	});
-
-	const Note = mongoose.model("Note", notesSchema);
-
-	app.get("/", function(req, res) {
-		res.send("express is working")
-		
-		// res.sendFile(__dirname + "index.html");  --->> to be used when 'express is working'
-	})
-
-	app.post("/", function(req, res) {
-		let newNote = new Note({
-			Word: req.body.word,
-			Language: req.body.langauge,
-			Gender: req.body.gender 
-		})
-
-		newNote.save();
-		res.redirect("/")
-	})
-
-
-// End of my nonsense 
-
 app.listen(
-	config.port,
-	() =>
-		config.isDev &&
-		console.log("Server is listening on http://localhost:" + config.port)
+  config.port,
+  () =>
+    config.isDev &&
+    console.log("Server is listening on http://localhost:" + config.port)
 );
+
+console.log(process);
